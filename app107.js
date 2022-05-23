fecd0ffee6f76dd287c5e9ced6c947118fceeb8d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-05-23 16:27:07 +0700

    update

---
+++ app107.js
@@ -0,0 +1,14 @@
+let x = 5
+let y = 3;
+
+//remainder
+console.log(`x % y = ${x % y}`)
+
+//Muliplication
+console.log(`x * y = ${x * y}`)
+
+//subtraction
+console.log(`x - y = ${x - y}`)
+
+//addition
+console.log(`x + y = ${x + y}`)
