4f37b299173ed17ee15d3cbf3915668ad05b6929
patipan-patisampita <patipan.patisampita@gmail.com>
2022-05-23 16:51:30 +0700

    update

---
+++ app108.js
@@ -0,0 +1,18 @@
+//Assignment[=]
+let x = 20
+//console.log(x)//20
+
+//Addition Assignment[+=]
+let y = 20
+y += 10 //y = y + 10
+//console.log(y)
+
+//Subtraction Assignment[-=]
+let z = 20
+z -= 10 //z = z-10
+//console.log(z)
+
+//Multipliction Assignment[-=]
+let a = 20
+a *= 10 //a = a*10 
+console.log(a)
